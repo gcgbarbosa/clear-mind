40e8f7b8a450ac6f1bb502f97a8913636970a0a2
gcgbarbosa <gcgbarbosa@gmail.com>
2023-05-03 23:12:17 -0700

    Bootstrap

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "react/no-unescaped-entities": "off",
+        "@next/next/no-page-custom-font": "off",
+        "react/react-in-jsx-scope": "off"
+    }
+}
