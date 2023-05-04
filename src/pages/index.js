9f5581857cb4cc9ff69d61552fb06db1850105c3
gcgbarbosa <gcgbarbosa@gmail.com>
2023-05-04 09:08:23 -0700

    Hello world anime

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,19 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
-import styles from '@/components/styles/Home.module.css'
-// import Image from 'next/image'
-
-const inter = Inter({ subsets: ['latin'] })
+import { useEffect } from 'react'
+import anime from "animejs"
 
 export default function Home() {
+  useEffect(() => {
+    anime({
+      targets: 'body',
+      translateX: 250,
+      rotate: '1turn',
+      backgroundColor: '#CCC',
+      duration: 800
+    });
+
+  }, [])
+
   return (
     <>
       <Head>
@@ -14,9 +22,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        Hello :)
-      </main>
     </>
   )
 }
