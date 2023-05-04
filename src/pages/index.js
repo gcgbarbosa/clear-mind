f24ee10deac94f9935fd742129859d316596a7d5
gcgbarbosa <gcgbarbosa@gmail.com>
2023-05-04 10:01:58 -0700

    Moving trough

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,16 @@
 import Head from 'next/head'
 import { useEffect } from 'react'
 import anime from "animejs"
+import { Container, Typography, Box, Paper } from '@mui/material';
 
 export default function Home() {
   useEffect(() => {
     anime({
-      targets: 'body',
+      targets: 'hello',
       translateX: 250,
       rotate: '1turn',
       backgroundColor: '#CCC',
-      duration: 800
+      duration: 5000
     });
 
   }, [])
@@ -22,6 +23,28 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Container maxWidth="sm">
+        <Box sx={
+          {
+            display:'flex',
+            flexDirection: 'column',
+            p:1,
+            m:1,
+            borderRadius:1,
+          }}>
+
+          <Typography id='hello' variant='h1'>Clear Mind</Typography>
+          <Box>
+            <Paper>
+              p1
+            </Paper>
+          </Box>
+
+          <Box sx={{marginTop:1}}>
+            <Paper>p2</Paper>
+          </Box>
+        </Box>
+      </Container>
     </>
   )
 }
